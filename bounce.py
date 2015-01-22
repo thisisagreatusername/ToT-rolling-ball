c59fadcd533ef11197e2eeb67afd54e683ca80fb
Henrik <toomie@arcor.de>
2015-01-22 21:37:28 +0100

    Bewegung mit am Rand-Abprall-Funktion
    Ich habe die Bewegung der Kugel, als Koordinaten im 2-Dimensionalem Raum
    aufgefasst und Python gibt die Koordinaten aus.
    Wenn der Ball an eine Wand kommt prallt er ab.
    Das Ganze lääst sich natürlich ganz einfach für ein 3-Dimensionales
    Koordinaten-System anpassen, wenn der Ball auch von Boden und Decke
    abprallen soll.
    Es wäre vll sinnvoll noch etwas Zufall mit hineinzubringen um dein Weg
    der Kugel interessanter zu gestalten.

---
+++ bounce.py
@@ -0,0 +1,36 @@
+from time import sleep
+
+
+#Raumgröße
+xlength = 10
+ylenght = 20
+
+#Start-Koordinaten
+# X und Y Koordinaten zu beginn
+coords = [0, 0]
+
+#Richtungs-Vektor
+#Vershiebung in X und Y-Richtung
+v = [1, 2]
+
+
+
+running = True
+print(coords)
+while running==True:
+    coords[0] = coords[0] + v[0]
+    coords[1] = coords[1] + v[1]
+    print(coords)
+    sleep(1)
+    #Wenn das Objekt an die obere Begrenzung in X-Richtung stößt prallt es ab
+    if coords[0] >= xlength:
+        v[0] = v[0] * -1
+    #Wenn das Objekt an die obere Begrenzung in Y-Richtung stößt prallt es ab
+    if coords[1] >= ylenght:
+        v[1] = v[1] * -1
+    #Wenn das Objekt an die untere Begrenzung in X-Richtung stößt prallt es ab
+    if coords[0] <= 0:
+        v[0] = v[0] * -1
+    #Wenn das Objekt an die untere Begrenzung in Y-Richtung stößt prallt es ab
+    if coords[1] <= 0:
+        v[1] = v[1] * -1
